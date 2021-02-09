2ce40a6f0ad6c66f4a73160f002ca090c9ae5b17
Adnan Latif <alatif.bwp@gmail.com>
2021-02-09 14:19:59 +0500

    Linked create exercise with backend

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -22,9 +23,13 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ['test user'],
-      username: 'test user',
+    axios.get('http://localhost:5000/users').then((response) => {
+      if (response.data.length > 0) {
+        this.setState({
+          users: response.data.map((user) => user.username),
+          username: response.data[0].username,
+        });
+      }
     });
   }
 
@@ -61,6 +66,10 @@ export default class CreateExercise extends Component {
 
     console.log(exercise);
 
+    axios
+      .post('http://localhost:5000/exercises/add', exercise)
+      .then((res) => console.log(res.data));
+
     window.location = '/';
   }
   render() {
@@ -77,13 +86,11 @@ export default class CreateExercise extends Component {
               value={this.state.username}
               onChange={this.onChangeUsername}
             >
-              {this.state.users.map(function (user) {
-                return (
-                  <option key={user} value={user}>
-                    {user}
-                  </option>
-                );
-              })}
+              {this.state.users.map((user) => (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              ))}
             </select>
           </div>
           <div className='form-group'>
